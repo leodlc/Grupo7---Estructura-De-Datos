afdcb518cb6c307f712301378385b364847f79f8
LeoDLC <leowowdlc@hotmail.com>
2022-02-10 16:19:59 -0500

    avance del proyecto 10-02-2022

---
+++ GRUPO7 - ESTRCUTURA DE DATOS/PROYECTO/v1/diccionario_edd/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Asrock/Documents/SEMESTRE OCT21-MAR22/Estructura de datos/Tercer Parcial/Proyecto v2/diccionario_edd
+# Build directory: C:/Users/Asrock/Documents/SEMESTRE OCT21-MAR22/Estructura de datos/Tercer Parcial/Proyecto v2/diccionario_edd/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
